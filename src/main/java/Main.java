4c288895bf59b387722d107c0582d7f7bb913088
Maria Osipova <71780020+sesterca@users.noreply.github.com>
2022-04-03 18:53:41 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String [] args){
-        System.out.println("Hello world! QA.guru");
+        System.out.println("Hello world! QA.GURU");
         System.out.println("How are you?");
     }
+    
+    public static void(){
+    System.out.println("Goodbuy!")}
 }
