689abc2da5ed7ef24ea005b11dc736bbbb92b07a
Maria Osipova <sesterca.mariaosipova@yandex.ru>
2022-04-03 18:59:23 +0300

    added question

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String [] args){
         System.out.println("Hello world! QA.guru");
         System.out.println("How are you?");
+        System.out.println("What is the weather like today?");
     }
 }
